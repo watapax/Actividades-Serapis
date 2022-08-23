f2ca3ee9bc89e5fa1af68b1b0c916a7786ed3054
Waro <38060212+warodot@users.noreply.github.com>
2022-08-22 23:01:53 -0400

    Mejoras sistema de ancla

--- Assets/Ambientes/Material Ambientes/Scripts/ZonaAncla.cs
+++ Assets/Ambientes/Material Ambientes/Scripts/ZonaAncla.cs
@@ -5,23 +5,48 @@ using UnityEngine;
 
 public class ZonaAncla : MonoBehaviour
 {
-
+    public UnityEvent m_MyEvent;
 
     public string anclaType;
     public string anclaName;
     public Sprite anclaThumbnail;
 
+    private bool playerCanClick = false;
+
+
+    void Start()
+    {
+        if (m_MyEvent == null)
+            m_MyEvent = new UnityEvent();
+        //StartCoroutine(WaitAndEnableClick);
+    }
+
+     void Update()
+    {
+        if (playerCanClick && Input.GetMouseButtonDown(0) && m_MyEvent != null)
+        {
+            m_MyEvent.Invoke();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         HUD.Instance.ShowAnclaInfo();
         HUD.Instance.SetAnclaInfo(anclaType,anclaName, anclaThumbnail);
+        StartCoroutine(WaitAndEnableClick(0.5f, true));
     }
 
     private void OnTriggerExit(Collider other)
     {
         HUD.Instance.HideAnclaInfo();
+        StartCoroutine(WaitAndEnableClick(0.01f,false));
     }
 
+    private IEnumerator WaitAndEnableClick(float waitTime, bool newValue)
+    {
+        yield return new WaitForSeconds(waitTime);
+        playerCanClick = newValue;
+
+    }
        
 }
