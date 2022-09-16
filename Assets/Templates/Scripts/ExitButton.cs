64f1b4dbb8aec157bb5f97809ee6ffa83375fee3
Waro <38060212+warodot@users.noreply.github.com>
2022-09-15 22:15:08 -0300

    Conectadas las zonas que hay
    Tambien hice unas mejoras al sistema para "volver" a otra escena. Se demoraba mucho el testeo asi que ahora es diferente.

--- Assets/Templates/Scripts/ExitButton.cs
+++ Assets/Templates/Scripts/ExitButton.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class ExitButton : MonoBehaviour
 {
+    
+
     //Este script se usa en los botones de "volver", llama al ManagerEscenas para que vuelva a la escena anterior.
     //Se conecta solo, para simplificar las cosas.
 
@@ -19,6 +21,7 @@ public class ExitButton : MonoBehaviour
     public void OnButtonPressed()
     {
         button.interactable = false;
+        //ManagerEscenas.Instance.CargarEscena(nombreEscenaDestino, "", IDAnclaDestino, "ZONA");
         ManagerEscenas.Instance.CargarUltimaEscena();
     }
 }
